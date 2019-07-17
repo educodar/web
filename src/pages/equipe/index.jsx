4b728d3d66b6800ad7a16149e43b7275b440af77
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-07-17 14:49:50 -0300

    Added contributing instructions, team pages change

--- src/pages/equipe/index.jsx
+++ src/pages/equipe/index.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 
 export default () => (
-    <div>
-        <h1>Educodar Belo Horizonte</h1>
+    <article>
+        <header>
+            <h1>Quem faz o Educodar</h1>
+        </header>
+        <h2>Educodar Belo Horizonte</h2>
         <ul>
             <li>Leandro Victor (Belo Horizonte)</li>
             <li>Luciano Borges (Belo Horizonte)</li>
@@ -10,19 +13,19 @@ export default () => (
             <li>Thais Nunes (Belo Horizonte)</li>
             <li>Vagner dos Santos (Belo Horizonte)</li>
         </ul>
-        <h1>Site Educodar.com.br</h1>
+        <h2>Site Educodar.com.br</h2>
         <ul>
             <li>Daniel Nascimento (Belo Horizonte)</li>
             <li>Inajara Leppa (Porto Alegre)</li>
             <li>Jimmy Andrade (Belo Horizonte)</li>
             <li>Lucas Carvalhais (Belo Horizonte)</li>
         </ul>
-        <h1>Já colaboraram</h1>
+        <h2>Já colaboraram</h2>
         <ul>
             <li>André Araújo (Belo Horizonte)</li>
             <li>Bruno Castelo (Belo Horizonte)</li>
             <li>Nayara Souza (Belo Horizonte)</li>
             <li>Paulo Gama (Recife)</li>
         </ul>
-    </div>
+    </article>
 );
