1c31f5187d61108d4e60b60f99267805d496df80
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-07-17 11:08:46 -0300

    Added team page & updated humans.txt

---
+++ src/pages/equipe/index.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default () => (
+    <div>
+        <h1>Educodar Belo Horizonte</h1>
+        <ul>
+            <li>Leandro Victor (Belo Horizonte)</li>
+            <li>Luciano Borges (Belo Horizonte)</li>
+            <li>Maria Simas (Belo Horizonte)</li>
+            <li>Thais Nunes (Belo Horizonte)</li>
+            <li>Vagner dos Santos (Belo Horizonte)</li>
+        </ul>
+        <h1>Site Educodar.com.br</h1>
+        <ul>
+            <li>Daniel Nascimento (Belo Horizonte)</li>
+            <li>Inajara Leppa (Porto Alegre)</li>
+            <li>Jimmy Andrade (Belo Horizonte)</li>
+            <li>Lucas Carvalhais (Belo Horizonte)</li>
+        </ul>
+        <h1>Já colaboraram</h1>
+        <ul>
+            <li>André Araújo (Belo Horizonte)</li>
+            <li>Bruno Castelo (Belo Horizonte)</li>
+            <li>Nayara Souza (Belo Horizonte)</li>
+            <li>Paulo Gama (Recife)</li>
+        </ul>
+    </div>
+);
