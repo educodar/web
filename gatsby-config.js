8248e02d365ced6644b05f7db48c0a47ce53df3e
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-07-25 21:52:29 +0000

    Added more directives

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         headers: {
           "/*": [
             "Content-Security-Policy: base-uri 'self'; default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; object-src 'none'; form-action 'self'; font-src 'self' data:; connect-src 'self'; img-src 'self' data:;",
-            "Feature-Policy: camera 'none'; fullscreen 'none'; geolocation 'self'; gyroscope 'self'; magnetometer 'none'; microphone 'none'; midi 'none'; notifications 'self'; payment 'none'; push 'self'; speaker 'self'; sync-xhr 'self'; usermedia 'self'; vibrate 'self'",
+            "Feature-Policy: ambient-light-sensor 'self'; autoplay 'self'; camera 'none'; display-capture 'self'; document-domain 'none'; encrypted-media 'self'; fullscreen 'none'; geolocation 'self'; gyroscope 'self'; magnetometer 'none'; microphone 'none'; midi 'none'; notifications 'self'; payment 'none'; picture-in-picture: 'none'; push 'self'; speaker 'self'; sync-xhr 'self'; usb 'none'; usermedia 'self'; vibrate 'self'; wake-lock 'none'; vr 'none'; xr 'none'",
             "Referrer-Policy: no-referrer",
             "X-Content-Type-Options: nosniff",
             "X-Frame-Options: DENY",
