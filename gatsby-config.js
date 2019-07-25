41738162ca242964eb5356b1743036e70d02d25b
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-07-25 21:27:34 +0000

    Added unsafe-inline to CSP for script-src

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
       options: {
         disableOnDev: false,
         directives: {
+          "script-src": "'self' 'unsafe-inline'",
           "style-src": "'self' 'unsafe-inline'",
         },
       },
