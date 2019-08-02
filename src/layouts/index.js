47a4d7b5a30aac21ffa0c5be2d123b4f9ed81809
marcos.bezerra <marcosbgomes@gmail.com>
2019-08-02 19:57:52 +0000

    Style and basic Html structure of homepage,
    Co-authored-by: Jimmy Andrade <contato@jimmyandrade.com>

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,12 +1,64 @@
 import React from "react"
+import "./index-stylesheet.css"
 
 export default ({ children }) => (
   <>
     <header>
-      <h1>Educodar</h1>
-      <p>Aulas de programação grátis para jovens</p>
+      <h1 id="logo">
+        <a href="#">Educod@r</a>
+      </h1>
+      <nav></nav>
     </header>
+    <div className="jumbotron">
+      <h1>Aulas de programação grátis para jovens</h1>
+      <p>
+        <button className="btn">Button 1</button>
+        <button className="btn">Button 2</button>
+      </p>
+    </div>
+    <div>
+      <ul className="striped-list">
+        <li>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. <a href="#">Veja mais.</a>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. <a href="#">Veja mais.</a>
+            </p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. <a href="#">Veja mais.</a>
+            </p>
+          </div>
+        </li>
+      </ul>
+    </div>
     <main>{children}</main>
-    <footer></footer>
+    <footer>
+      <p>ThoiughtWorks</p>
+    </footer>
   </>
 )
