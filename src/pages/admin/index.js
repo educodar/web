de34fb415d98e8748ca86d852c7cb1595e129425
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-12-28 12:57:34 -0300

    chore(admin/index.js) Remove broken admin page

--- src/pages/admin/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { Helmet } from "react-helmet"
-
-export default () => (<><Helmet>
-  <script src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"/>
-  <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-</Helmet></>)
