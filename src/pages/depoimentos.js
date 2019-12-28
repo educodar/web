5c3a294d4434faefdc63337046582ce6fba9cbf2
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-12-28 12:56:27 -0300

    feat(depoimentos.js) Add TestimonialsPage

---
+++ src/pages/depoimentos.js
@@ -0,0 +1,72 @@
+import React from "react"
+
+export default () => <article>
+  <h1>Depoimentos</h1>
+  <section>
+    <h2>Estudantes do Pré-Vestibular do Morro do Papagaio</h2>
+    <blockquote><p>Amei toda a equipe. Estão de parabéns. Desejo sucesso.</p>
+      <footer>Jhenifer</footer>
+    </blockquote>
+    <blockquote><p>Achei perfeito! Me ajudou a tirar muitas dúvidas sobre a área que quero trabalhar.</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote><p>Ótimo lugar com profissionais incríveis. Amei a experiência. Beijos!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote><p>Tudo perfeito. Amei de coração e espero ter mais oportunidades. Com amor,</p>
+      <footer>Vitoria Braga</footer>
+    </blockquote>
+    <blockquote>
+      <p>Vocês não precisam melhorar nada, o Educodar é maravilhoso, adorei conhecer as pessoas que fazem parte dele. Adorei. Beijos!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Gostei de tudo: das pessoas do projeto e de como nós fomos tratadas. Parabéns!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Eu gostei muito, principalmente das pessoas que foram muito atenciosas com a gente. Estão de parabéns!</p>
+      <footer>Carol Line</footer>
+    </blockquote>
+    <blockquote>
+      <p>O que foi legal para mim foi tudo, as pessoas, o lugar, o diferencial. Vocês são incríveis!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Gostei de tudo, pessoal super antencioso, o projeto é maravilhoso e não precisa mudar nada até então.</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Adorei a experiência! Aprendi bastante coisa que não sabia, e achei o projeto maravilhoso! A <em lang="en">vibe</em> é ótima. Não tenho palavras pra descrever o quanto gostei, e espero que o projeto cresça ainda mais.</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Adorei! Vocês são muito comunicativas. Explicaram muito bem todas as dúvidas. Alto astral o projeto. <abbr lang="en" title="Beijos e abraços">XOXO</abbr>!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+    <blockquote>
+      <p>Pra mim tudo foi legal, adorei conhecer como funciona e adorei as pessoas que trabalham aqui. Vocês são massa!</p>
+      <footer>Anônima</footer>
+    </blockquote>
+  </section>
+  <section>
+    <h2>Turma de 2019</h2>
+    <blockquote><p>Não tenho reclamações a fazer, só elogios. É ótimo o método de ensino, aprendendo a parte teórica ao
+      mesmo tempo em que praticamos. Sem contar no quanto é bom não apenas aprender o conteúdo, mas também conhecer a
+      rotina de trabalho, entender como é feita a dinâmica entre equipes, saber como proceder, as etapas de cada
+      serviço.</p><p>Apenas acredito que o tempo para ensinamento do conteúdo poderia ser melhor administrado. Vimos
+      muito de <abbr>HTML</abbr> e pouco de <abbr>CSS</abbr> e JavaScript. Fora isso, está perfeito.</p>
+      <footer>Anônimo</footer>
+    </blockquote>
+    <blockquote>
+      <p>As aulas do Educodar em geral são bem dinâmicas, tranquilas, divertidas. Porém, acredito que os temas <abbr>HTML</abbr> e <abbr>CSS</abbr> podem vir juntos para melhor aproveitamento. Para criar meu portfólio estou usando o máximo das aulas para fazer o melhor.</p>
+      <p>Espero que dê bastante tempo para vermos também JavaScript.</p>
+      <p>Desde já agradeço pela oportunidade.</p>
+      <footer>Anônimo</footer>
+    </blockquote>
+    <blockquote>
+      <p>As aulas do Educodar estão sendo muito boas. Todas as pessoas que passam para nos dar aulas são ótimas, sempre aprendemos e relembramos coisas novas. Pela turma: estamos desenvolvendo muito bem. Estou amando o curso.</p>
+      <footer>Jéssica</footer>
+    </blockquote>
+  </section>
+</article>
