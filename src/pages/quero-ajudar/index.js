fb928113f8809d1c307e6d034dd9c5564c6017d7
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-12-28 12:56:59 -0300

    feat(quero-ajudar/index.js) Add How to Help page

---
+++ src/pages/quero-ajudar/index.js
@@ -0,0 +1,30 @@
+import React from "react"
+
+export default () => (
+  <article>
+    <h1>Como ajudar</h1>
+    <ul>
+      <li>
+        Facilitação de <em lang="en">workshops</em>
+      </li>
+      <li>
+        Facilitação de aulas
+      </li>
+      <li>
+        Suporte
+      </li>
+      <li>
+        Compra de lanche
+      </li>
+      <li>
+        Gestão de orçamento
+      </li>
+      <li>
+        Divulgação nas redes sociais
+      </li>
+      <li>
+        Desenvolvimento do site
+      </li>
+    </ul>
+  </article>
+)
