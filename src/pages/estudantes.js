5b99e32facab5f60765414f19bd52fbbaaee0a1a
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2019-12-28 12:56:02 -0300

    feat(turmas.js) Add StudentsPage

---
+++ src/pages/estudantes.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const StudentsPage = () => (
+  <article><ul>
+    <li>Guilherme (convidado após participar <em lang="en">workshop</em>);</li>
+    <li>Gustavo (foi indicado pelo Guilherme)</li>
+    <li>Jéssica (foi indicada pela empresa ThoughtWorks)</li>
+    <li>Joás (foi indicado pelo Guilherme)</li>
+  </ul>
+  </article>
+)
+
+export default StudentsPage;
