d182bf790093c8dd41ab34c504fa2c44d6bfca82
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-11 00:32:24 -0300

    tests(jest.config.js) ignore cypress on unit tests
    Include /cypress on coveragePathIgnorePatterns and testPathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: "./coverage/",
-  coveragePathIgnorePatterns: ["/.cache/", "/node_modules/"],
+  coveragePathIgnorePatterns: ["/.cache/", "/cypress/", "/node_modules/"],
   globals: {
     __PATH_PREFIX__: ``,
   },
@@ -10,7 +10,7 @@ module.exports = {
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   setupFiles: [`<rootDir>/loadershim.js`],
-  testPathIgnorePatterns: [`node_modules`, `.cache`],
+  testPathIgnorePatterns: [".cache", "cypress", "node_modules"],
   testURL: `http://localhost:8000`,
   transform: {
     "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
