233c6ac3168e6beb92218452b79461c41fd3c7ea
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-12 20:32:21 -0300

    fix(gatsby-config.js): update script-src-elem CSP header

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ const contentSecurityPolicy = [
   `img-src 'self' data:;`,
   `object-src 'none';`,
   `script-src 'self' 'unsafe-inline' 'unsafe-eval';`,
-  `script-src-elem 'self' 'unsafe-inline' identity.netlify.com unpkg.com;`,
+  `script-src-elem 'self' 'unsafe-inline' cdn.lr-ingest.io identity.netlify.com unpkg.com;`,
   `style-src 'self' 'unsafe-inline';`,
 ]
 
