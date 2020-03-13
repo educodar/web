1c6d55361e3008b753d4fe71ffd46bd4403c5d66
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-13 02:20:53 -0300

    chore(gatsby-config.js): add todo for productionSiteUrl refactor

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ const contentSecurityPolicy = [
   `worker-src blob: data:`,
 ]
 
+// @todo Move productionSiteUrl value to .env (if viable)
 const productionSiteUrl = `https://educodar.com.br`
 
 module.exports = {
