28c78a21bad781f9e9513f738a0475fa4d2d2bf2
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-13 02:01:59 -0300

    chore(gatsby-browser.js): add security todo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,6 @@ require("./src/index")
 const LogRocket = require("logrocket")
 
 exports.onClientEntry = () => {
+  // @todo Move LogRocket init data to environment variable
   LogRocket.init("9iy3ll/educodar")
 }
