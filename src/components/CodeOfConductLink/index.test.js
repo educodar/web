85147968de4d7cd61cfe28e8829bca7250245105
Vinícius Ponciano <vinicius.ponciano@thoughtworks.com>
2020-03-19 19:26:48 -0300

    test(codeofconductlink): updated CodeOfConductLink tests and snapshot

--- src/components/CodeOfConductLink/index.test.js
+++ src/components/CodeOfConductLink/index.test.js
@@ -3,8 +3,13 @@ import renderer from "react-test-renderer"
 import ConductCodeLink from "."
 
 describe("Conduct Code Link component", () => {
-  it("renders correctly", () => {
+  it("renders correctly without custom props", () => {
     const tree = renderer.create(<ConductCodeLink />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("renders correctly with openInNew props", () => {
+    const tree = renderer.create(<ConductCodeLink openInNew />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
