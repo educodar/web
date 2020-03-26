b4199da2ca066b1bc7aaa8cd7bdf00f8c8a365a7
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-03-26 00:39:06 -0300

    test(layouts/index): mock useSiteMetadata hook

--- src/layouts/index.test.js
+++ src/layouts/index.test.js
@@ -3,6 +3,9 @@ import renderer from "react-test-renderer"
 import Layout from "."
 
 describe("Default layout", () => {
+  beforeAll(() => {
+    jest.mock("../hooks/useSiteMetadata")
+  })
   it("renders correctly", () => {
     const tree = renderer.create(<Layout>Content</Layout>).toJSON()
     expect(tree).toMatchSnapshot()
